f890631bdc18992dbdee6e9f483fc2beee41a81f
Levi Zitting <me@levizitting.com>
2026-02-19 11:45:49 -0600

    feat: implement writeonly_data resource

--- internal/provider/data_resource.go
+++ internal/provider/data_resource.go
@@ -5,11 +5,15 @@ import (
 	"math/rand"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var _ resource.Resource = &DataResource{}
-var _ resource.ResourceWithImportState = &DataResource{}
 
 func NewDataResource() resource.Resource {
 	return &DataResource{}
@@ -20,7 +24,7 @@ type DataResource struct{}
 type DataResourceModel struct {
 	Id              types.String `tfsdk:"id"`
 	InputWo         types.String `tfsdk:"input_wo"`
-	InputWoVersion  types.Number `tfsdk:"input_wo_version"`
+	InputWoVersion  types.Int64  `tfsdk:"input_wo_version"`
 	TriggersReplace types.List   `tfsdk:"triggers_replace"`
 	Output          types.String `tfsdk:"output"`
 }
@@ -30,26 +34,108 @@ func (r *DataResource) Metadata(ctx context.Context, req resource.MetadataReques
 }
 
 func (r *DataResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"input_wo": schema.StringAttribute{
+				Optional:  true,
+				WriteOnly: true,
+			},
+			"input_wo_version": schema.Int64Attribute{
+				Optional: true,
+				Computed: true,
+				PlanModifiers: []planmodifier.Int64{
+					int64planmodifier.UseStateForUnknown(),
+				},
+			},
+			"triggers_replace": schema.ListAttribute{
+				ElementType: types.StringType,
+				Optional:    true,
+				PlanModifiers: []planmodifier.List{
+					listplanmodifier.RequiresReplace(),
+				},
+			},
+			"output": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
 }
 
 func (r *DataResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 }
 
 func (r *DataResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan DataResourceModel
+	diags := req.Config.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
+	if plan.Id.IsNull() {
+		plan.Id = types.StringValue(generateRandomID())
+	}
+
+	if !plan.InputWo.IsNull() {
+		plan.Output = plan.InputWo
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
 }
 
 func (r *DataResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state DataResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, state)...)
 }
 
 func (r *DataResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan DataResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var state DataResourceModel
+	diags = req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	versionChanged := !plan.InputWoVersion.Equal(state.InputWoVersion)
+
+	if versionChanged {
+		var config DataResourceModel
+		diags = req.Config.Get(ctx, &config)
+		resp.Diagnostics.Append(diags...)
+		if resp.Diagnostics.HasError() {
+			return
+		}
+		plan.Output = config.InputWo
+	} else {
+		plan.Output = state.Output
+	}
+
+	plan.Id = state.Id
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, plan)...)
 }
 
 func (r *DataResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-
-}
-
-func (r *DataResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	req.State.RemoveResource(ctx)
 }
 
 func generateRandomID() string {
